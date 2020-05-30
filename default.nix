841b211b7e95d4436309a751f978b43ff912dbc9
Aaron L. Zeng <me@bcc32.com>
2020-05-30 15:40:33 -0400

    Build with nix

---
+++ default.nix
@@ -0,0 +1,13 @@
+with import <nixpkgs> { };
+
+let
+  inherit (ocamlPackages)
+    buildDunePackage async async_interactive core core_bench re shexp;
+
+in buildDunePackage rec {
+  pname = "snapsend";
+  version = "0.1.0";
+  src = lib.cleanSource ./.;
+  propagatedBuildInputs = [ async async_interactive core re shexp ];
+  meta = { homepage = "https://github.com/bcc32/snapsend"; };
+}
