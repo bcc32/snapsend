b2de1c5d7db04e631dc3bf3fcd5aefaae8fbad2f
Aaron L. Zeng <me@bcc32.com>
2021-01-20 23:20:50 -0500

    Fix nix build

--- default.nix
+++ default.nix
@@ -7,6 +7,7 @@ let
 in buildDunePackage rec {
   pname = "snapsend";
   version = "0.1.0";
+  useDune2 = true;
   src = nix-gitignore.gitignoreFilterSource lib.cleanSourceFilter [ ] ./.;
   propagatedBuildInputs = [ async async_interactive core re shexp ];
   meta = { homepage = "https://github.com/bcc32/snapsend"; };
