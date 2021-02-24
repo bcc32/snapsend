a4b63dcad9a957d1a633681040eaeaf97b384a57
Aaron L. Zeng <me@bcc32.com>
2021-02-24 01:02:13 -0500

    Remove lockfile support
    There is no need for this, since btrfs receive will simply refuse to
    create a subvolume that already exists.  The worst thing that happens
    is an error message.

--- bin/send.ml
+++ bin/send.ml
@@ -16,11 +16,5 @@ let command =
      fun () ->
        let open Deferred.Or_error.Let_syntax in
        let%bind configs = Snapsend.Config.read_from_file config_path in
-       let lock_path = config_path ^ ".lock" in
-       let%bind () =
-         if%map.Deferred Lock_file_async.create ~unlink_on_exit:true lock_path
-         then Ok ()
-         else Or_error.error_s [%message "couldn't acquire lockfile" (lock_path : string)]
-       in
        configs |> Deferred.Or_error.List.iter ~how:`Parallel ~f:Snapsend.sync)
 ;;
