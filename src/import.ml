29b9b9bbf267b12acf7a6518011200b7a24a525d
Aaron L. Zeng <me@bcc32.com>
2021-02-24 20:11:19 -0500

    Use compact sexp logging format

--- src/import.ml
+++ src/import.ml
@@ -3,4 +3,4 @@ open! Async
 include Ppx_log_async
 include Deferred.Or_error.Let_syntax
 
-let () = Log.Global.set_output [ Log.Output.stdout ~format:`Sexp_hum () ]
+let () = Log.Global.set_output [ Log.Output.stdout ~format:`Sexp () ]
